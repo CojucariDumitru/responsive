69723f484b0711daddd52fe3f197ffb43994ac21
Dumitru Cojucari <105176173+CojucariDumitru@users.noreply.github.com>
2022-05-29 21:34:26 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const responsive = {
+    0:{
+        items: 1
+    },
+    320:{
+        items: 1
+    },
+    560:{
+        items: 2
+    },
+    960:{
+        items: 3
+    }
+}
+
+$(document).ready(function(){
+
+    $nav=$('.nav');
+    $toggleCollapse=$('.toggle-collapse')
+
+    /**click event on toggle menu */
+    $toggleCollapse.click(function(){
+        $nav.toggleClass('collapse');
+    });
+
+//owl-carousel
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    autoplayTimeout: 3000,
+    autoplay: true,
+    responsive: responsive
+});
+
+//Click to scroll top
+$('.move-up span').click(function () {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 1000);
+})
+});
